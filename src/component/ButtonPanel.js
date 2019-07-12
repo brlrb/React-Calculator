d47fa48569d2d9e1e6ca9fb9a46a3d31c05a1b51
brlrb <bikramcode@gmail.com>
2019-07-11 23:01:58 -0700

    Added buttons for scientific computation

--- src/component/ButtonPanel.js
+++ src/component/ButtonPanel.js
@@ -45,6 +45,11 @@ export default class ButtonPanel extends React.Component {
           <Button name="." clickHandler={this.handleClick} />
           <Button name="=" clickHandler={this.handleClick} orange />
         </div>
+        <div>
+          <Button name="SIN" clickHandler={this.handleClick} wide />
+          <Button name="COSINE" clickHandler={this.handleClick} />
+          <Button name="TAN" clickHandler={this.handleClick}  />
+        </div>
       </div>
     );
   }
